60ae39b3bd9d74decdbe6d7f1d5dc83aa79856dc
c4ssio <cassio.paesleme@gmail.com>
2013-10-01 10:27:34 -0700

    finished historicaldata script

--- scripts/historicaldata.rb
+++ scripts/historicaldata.rb
@@ -1,7 +1,11 @@
 require 'mechanize'
 require 'csv'
+require 'json'
 tickers_path = "gsheet/mobilize_server_gmail_com/Mobilize Sample Jobs/tickers"
 tickers = CSV.read(tickers_path).flatten
+
+#output the metric header
+puts "date\tticker\t@metric\n"
 
 tickers.each do |ticker|
 
@@ -15,6 +19,10 @@ tickers.each do |ticker|
   agent = Mechanize.new
   file = agent.get("#{yql_prefix}#{yql_query}#{yql_suffix}")
 
+  hash_array = JSON.parse(file.body)
 
-
+  hash_array.each do |hash|
+    #output each data point
+    puts "#{hash['date']}\t#{ticker}\t#{hash['@metric']}\n"
+  end
 end
